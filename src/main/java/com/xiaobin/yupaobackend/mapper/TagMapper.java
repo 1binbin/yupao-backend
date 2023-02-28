c67d9c2562c6ba362afe0f2b4b84fc4b9419f339
hongxiaobin <2812181610@qq.com>
2023-02-28 22:15:36 +0800

    feat:
    生成与Tag相关的文件

---
+++ src/main/java/com/xiaobin/yupaobackend/mapper/TagMapper.java
@@ -0,0 +1,18 @@
+package com.xiaobin.yupaobackend.mapper;
+
+import com.xiaobin.yupaobackend.model.domain.Tag;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author hongxiaobin
+* @description 针对表【tag(标签)】的数据库操作Mapper
+* @createDate 2023-02-28 22:14:15
+* @Entity com.xiaobin.yupaobackend.model.domain.Tag
+*/
+public interface TagMapper extends BaseMapper<Tag> {
+
+}
+
+
+
+
---
+++ src/main/java/com/xiaobin/yupaobackend/service/TagService.java
@@ -0,0 +1,13 @@
+package com.xiaobin.yupaobackend.service;
+
+import com.xiaobin.yupaobackend.model.domain.Tag;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author hongxiaobin
+* @description 针对表【tag(标签)】的数据库操作Service
+* @createDate 2023-02-28 22:14:15
+*/
+public interface TagService extends IService<Tag> {
+
+}
